62c30edfe1b60a7e9e1de078d466956b780eb88d
LifeIsMadness <Luckstriker073@gmail.com>
2021-03-24 15:46:56 +0300

    changes

--- public/js/site.js
+++ public/js/site.js
@@ -1,8 +1,20 @@
 
-let _main = document.querySelector('main');
-let _body = document.querySelector('body');
-let bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
-while (window.innerHeight - bodyHeight > 0) {
-    _main.appendChild(document.createElement('br'));
-    bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
+function setFooter() {
+    let _main = document.querySelector('main');
+    let _body = document.querySelector('body');
+    let bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
+    console.log(window.innerHeight, bodyHeight);
+    while (window.innerHeight - bodyHeight > 0) {
+        _main.appendChild(document.createElement('br'));
+        console.log(window.innerHeight, bodyHeight);
+        bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
+    }
 }
+
+tId = setTimeout(setFooter, 500);
+
+window.addEventListener('resize',
+    () => {
+    clearTimeout(tId);
+    tId = setTimeout(setFooter, 500);
+});
