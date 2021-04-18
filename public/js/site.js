96f84fc8a01fa23d6687630e79b04f878c4103eb
LifeIsMadness <Luckstriker073@gmail.com>
2021-04-18 03:55:55 +0300

    local storage

--- public/js/site.js
+++ public/js/site.js
@@ -2,10 +2,10 @@ function setFooter() {
     let _main = document.querySelector('main');
     let _body = document.querySelector('body');
     let bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
-    console.log(window.innerHeight, bodyHeight);
+    // console.log(window.innerHeight, bodyHeight);
     while (window.innerHeight - bodyHeight > 0) {
         _main.appendChild(document.createElement('br'));
-        console.log(window.innerHeight, bodyHeight);
+        // console.log(window.innerHeight, bodyHeight);
         bodyHeight = window.getComputedStyle(_body).height.replace('px', '');
     }
 }
@@ -15,3 +15,5 @@ window.addEventListener('resize',
     setTimeout(setFooter, 500);
 });
 
+const token = 'https://api.jsonbin.io/v3/b/607a08295b165e19f62196cf/latest';
+const key = '$2b$10$pr6K8aK2mjCmm5vXuEJzw.mn99BW4mJHMveq9Wx3tUQRlvm3LAUkK';
